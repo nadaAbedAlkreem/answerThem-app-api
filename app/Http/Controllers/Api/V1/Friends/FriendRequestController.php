afb277e7d5b51f5a2c7465bbf36eefa6b695d305
nadaAbedAlkreem <102536104+nadaAbedAlkreem@users.noreply.github.com>
2024-11-13 11:53:00 +0200

    Update FriendRequestController.php

--- app/Http/Controllers/Api/V1/Friends/FriendRequestController.php
+++ app/Http/Controllers/Api/V1/Friends/FriendRequestController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\V1\Friends;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreFriendRequestRequest;
-use App\Http\Resources\Api\UserResource;
 use App\Models\Notification;
 use App\Repositories\Eloquent\FriendRequestRepository;
 use App\Services\FcmNotificationService;
