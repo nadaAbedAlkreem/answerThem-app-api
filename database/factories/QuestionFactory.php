753cf536bdddbf4338ccc906489e6af22c7d4443
nadaAbedAlkreem <102536104+nadaAbedAlkreem@users.noreply.github.com>
2024-11-14 13:12:18 +0200

    Update QuestionFactory.php

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Answer;
 use App\Models\Category;
 use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -23,7 +24,18 @@ class QuestionFactory extends Factory
         return [
             'category_id' => Category::factory(5),
             'question_ar_text' => $arabicFaker->sentence,
-            'question_en_text' => $this->faker->optional()->sentence,
+            'question_en_text' => $this->faker->sentence,
         ];
     }
+
+     public function configure()
+    {
+        return $this->afterCreating(function ($question) {
+             $answers = Answer::factory()->for($question)->count(4)->create();
+              $answers->random()->update(['is_correct' => true]);
+        });
+    }
+
+
+    
 }
