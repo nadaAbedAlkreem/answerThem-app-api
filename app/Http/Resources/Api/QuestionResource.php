30793b866bd2a4e938384188e03005c08f6a8d2d
nadaAbedAlkreem <102536104+nadaAbedAlkreem@users.noreply.github.com>
2024-11-14 13:19:19 +0200

    Update QuestionResource.php

--- app/Http/Resources/Api/QuestionResource.php
+++ app/Http/Resources/Api/QuestionResource.php
@@ -19,6 +19,7 @@ class QuestionResource extends JsonResource
          return [
             'question_text' => $locale === 'ar' ? $this->question_ar_text : $this->question_en_text,
             'answers' => AnswerResource::collection($this->answers),
+            'timer' => 60  ,
         ];
 
     }
