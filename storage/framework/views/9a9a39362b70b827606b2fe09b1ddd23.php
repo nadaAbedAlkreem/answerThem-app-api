7949341d38f0938a7566e85b3d24538d979021bf
nadaAbedAlkreem <elkahloutnada@gmail.com>
2024-12-15 12:10:57 +0200

    update Question

--- storage/framework/views/9a9a39362b70b827606b2fe09b1ddd23.php
+++ storage/framework/views/9a9a39362b70b827606b2fe09b1ddd23.php
@@ -27,37 +27,46 @@
                 <!--end::Header Logo-->
                 <!--begin::Toolbar wrapper-->
                 <div class="topbar d-flex align-items-stretch flex-shrink-0" id="kt_topbar">
-
-                    <!--begin::User account menu-->
-                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <div class="menu-content d-flex align-items-center px-3">
-                                <!--begin::Avatar-->
-                                <div class="symbol symbol-50px me-5">
-                                    <img alt="Logo" src="assets/media/avatars/300-1.jpg" />
+                    <!--begin::User-->
+                    <div class="d-flex align-items-center ms-2 ms-lg-3" id="kt_header_user_menu_toggle">
+                        <!--begin::Menu wrapper-->
+                        <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                            <img src="assets/media/avatars/300-1.jpg" alt="user" />
+                        </div>
+                        <!--begin::User account menu-->
+                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
+                            <!--begin::Menu item-->
+                            <div class="menu-item px-3">
+                                <div class="menu-content d-flex align-items-center px-3">
+                                    <!--begin::Avatar-->
+                                    <div class="symbol symbol-50px me-5">
+                                        <img alt="Logo" src="assets/media/avatars/300-1.jpg" />
+                                    </div>
+                                    <!--end::Avatar-->
+                                    <!--begin::Username-->
+                                    <div class="d-flex flex-column">
+                                        <div class="fw-bolder d-flex align-items-center fs-5">admin
+                                            <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2"></span></div>
+                                        <a href="#" class="fw-bold text-muted text-hover-primary fs-7">admin@gmail.com</a>
+                                    </div>
+                                    <!--end::Username-->
                                 </div>
-                                <!--end::Avatar-->
-                                <!--begin::Username-->
-                                <div class="d-flex flex-column">
-                                    <div class="fw-bolder d-flex align-items-center fs-5">admin
-                                        <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2"></span></div>
-                                    <a href="#" class="fw-bold text-muted text-hover-primary fs-7">admin@gmail.com</a>
-                                </div>
-                                <!--end::Username-->
                             </div>
+                            <!--end::Menu item-->
+
+
+                            <!--end::Menu item-->
+                            <!--begin::Menu item-->
+                            <div class="menu-item px-5">
+                                <a href="<?php echo e(route('admin.logout')); ?>" class="menu-link px-5">Sign Out</a>
+                            </div>
+
+                            <!--end::Menu item-->
                         </div>
-                        <!--end::Menu item-->
-
-
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-5">
-                            <a href="<?php echo e(route('admin.logout')); ?>" class="menu-link px-5">Sign Out</a>
-                        </div>
-
+                        <!--end::User account menu-->
+                        <!--end::Menu wrapper-->
                     </div>
-                    <!--end::User account menu-->
+                    <!--end::User -->
                     <!--begin::Heaeder menu toggle-->
                     <!--end::Heaeder menu toggle-->
                 </div>
