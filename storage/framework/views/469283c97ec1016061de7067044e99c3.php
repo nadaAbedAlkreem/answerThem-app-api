5b22814a64bd0ec75e09dce82828ed561c0f4b29
nadaAbedAlkreem <elkahloutnada@gmail.com>
2024-12-17 10:06:30 +0200

    update

---
+++ storage/framework/views/469283c97ec1016061de7067044e99c3.php
@@ -0,0 +1,16 @@
+<!--begin::Fonts-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+<!--end::Fonts-->
+<!--begin::Page Vendor Stylesheets(used by this page)-->
+<link href="<?php echo e(asset('assets/plugins/custom/datatables/datatables.bundle.css')); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo e(asset('assets/plugins/custom/vis-timeline/vis-timeline.bundle.css')); ?>" rel="stylesheet" type="text/css" />
+<!--end::Page Vendor Stylesheets-->
+<!--begin::Global Stylesheets Bundle(used by all pages)-->
+<link href="<?php echo e(asset('assets/plugins/global/plugins.bundle.css')); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo e(asset('assets/css/style.bundle.css')); ?>" rel="stylesheet" type="text/css" />
+<!--end::Global Stylesheets Bundle-->
+<link href="<?php echo e(asset('assets/plugins/global/plugins.bundle.css')); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo e(asset('assets/css/style.bundle.css')); ?>" rel="stylesheet" type="text/css" />
+
+
+<?php /**PATH C:\xampp\htdocs\answerThem-api-main\resources\views/dashboard/asset/css.blade.php ENDPATH**/ ?>
